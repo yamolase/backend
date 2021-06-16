c87201940da734492b54cc21746b7e0864c982f1
Yamolase <Yamolase15@gmail.com>
2021-06-16 23:13:12 +0700

    berhasil

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
+// const express =  require('express')
+// const app = express()
+
+// const cors = require('cors')
+// app.use(cors())
+
+// app.use(express.urlencoded({extended:true}))
+// app.use(express.json())
+
+// const mysql = require('mysql')
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "todolist"
+// });
+// con.connect(function(err){
+//     console.log(err);
+// });
+
+// app.get('/', (req,res) => {
+//     res.send(`
+//     <html>
+//         <body>
+//             <form action="/todo" method="post">
+//                 <input name="deskripsi" />
+//                 <button>Add</button>
+//             </form>
+//         </body>
+//     </html>`)
+// })
+
+// //insert to database
+// app.post('/todo',(req,res)=> {
+//     console.log("yamo")  
+//     const kata = req.body.deskripsi
+//     con.query("insert into tabel_todo(deskripsi) values (?)",kata)
+//     res.end() 
+// })
+ 
+
+// app.get('/todo', (req ,res) => {
+//     console.log("Lase")
+//     con.connect(function(err) {     
+        
+//         con.query("SELECT * FROM tabel_todo", function (err, result) {
+//             console.log(result)
+//             res.json(result)
+//             res.end()
+//         });
+//       });
+// })
+
+
+// app.delete('/todo/:id', (req, res)=> {
+//     console.log(id)
+//    // con.query('DELETE FROM tabel_todo WHERE id = ?', [req.params.id]); 
+//     con.query('DELETE FROM tabel_todo WHERE tabel_todo.id = ?', [req.params.id]); 
+//     res.end()
+//     console.log('terserah')
+// })
+
+// app.listen(3000,function() {
+//     console.log("server udah jalan ")
+// })
+
 const express =  require('express')
 const app = express()
 
@@ -14,9 +80,7 @@ var con = mysql.createConnection({
     password: "",
     database: "todolist"
 });
-con.connect(function(err){
-    console.log(err);
-});
+
 
 app.get('/', (req,res) => {
     res.send(`
@@ -31,32 +95,30 @@ app.get('/', (req,res) => {
 })
 
 //insert to database
-app.post('/todo',(req,res)=> {
-    console.log("yamo")  
-    const kata = req.body.deskripsi
-    con.query("insert into tabel_todo(deskripsi) values (?)",kata)
+app.post('/todo',(req,res)=> {  
+    con.connect(function(err){
+        const kata = req.body.deskripsi
+        con.query("insert into tabel_todo(deskripsi) values (?)",[kata])
+    })
     res.end() 
 })
- 
 
 app.get('/todo', (req ,res) => {
-    console.log("Lase")
     con.connect(function(err) {     
-        
+        var data ="" 
         con.query("SELECT * FROM tabel_todo", function (err, result) {
-            console.log(result)
+            console.log({id: result.id})
             res.json(result)
             res.end()
         });
       });
 })
 
-
-app.delete('/todo/delete', (req, res)=> {
-    con.query('DELETE FROM tabel_todo WHERE id = ?', [req.body.id]); 
+app.delete('/todo/:id', (req, res)=> {
+    con.query('DELETE FROM tabel_todo WHERE id = ?', [req.params.id]); 
     res.end()
 })
 
 app.listen(3000,function() {
-    console.log("server udah jalan ")
+    console.log("server udah jalan bos")
 })
